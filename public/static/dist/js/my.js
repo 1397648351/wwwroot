e9d34851d5d32304eac3ac64b0fc1d12a4f3032a
1397648351 <wuzedc@163.com>
2018-03-23 23:32:42 +0800

    首页

--- public/static/dist/js/my.js
+++ public/static/dist/js/my.js
@@ -1,35 +1,36 @@
 /**
- * 
+ *
  */
-(function($) {
-	$.extend({
-		my_init: function(option) {
-			var default_option = {
-				animate: {
-					ele: ".animated-end"
-				}
-			}
-			option = $.extend(default_option, option);
-			if(option.animate) {
-				$(option.animate.ele).setAnimate();
-			}
-		},
-	});
-	$.fn.extend({
-		setAnimate: function() {
-			var eles = this;
-			$(window).scroll(function(event) {
-				var top = $(window).scrollTop();
-				if(eles && eles.length > 0) {
-					for(var i = 0; i < eles.length; i++) {
-						var _top = $(window).height();
-						if(top + _top - eles[i].offsetTop >= $(eles[i]).height() - 80) {
-							$(eles[i]).removeClass('transparent')
-							$(eles[i]).children().addClass('animated fadeInUp')
-						}
-					}
-				}
-			});
-		},
-	});
+(function ($) {
+    $.extend({
+        my_init: function (option) {
+            var default_option = {
+                animate: {
+                    ele: ".animated-end"
+                }
+            }
+            option = $.extend(default_option, option);
+            $.setAnimate(option.animate);
+            $(window).scroll(function (event) {
+                if (option.animate) {
+                    $.setAnimate(option.animate);
+                }
+            });
+        },
+        setAnimate: function (animate) {
+            if (animate) {
+                var eles = $(animate.ele);
+                var top = $(window).scrollTop();
+                if (eles && eles.length > 0) {
+                    for (var i = 0; i < eles.length; i++) {
+                        var _top = $(window).height();
+                        if (top + _top - eles[i].offsetTop >= $(eles[i]).height() - 80) {
+                            $(eles[i]).removeClass('transparent')
+                            $(eles[i]).children().addClass('animated fadeInUp')
+                        }
+                    }
+                }
+            }
+        },
+    });
 })(jQuery);
