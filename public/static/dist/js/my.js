3588c6ee47b161e51eb2ed6ee56d9395301b1055
1397648351 <wuzedc@163.com>
2018-03-24 10:58:40 +0800

    首页

--- public/static/dist/js/my.js
+++ public/static/dist/js/my.js
@@ -1,14 +1,15 @@
 /**
- *
+ * 项目js文件，需要jQuery
  */
 (function ($) {
     $.extend({
+        /*初始化*/
         my_init: function (option) {
             var default_option = {
                 animate: {
                     ele: ".animated-end"
                 }
-            }
+            };
             option = $.extend(default_option, option);
             $.setAnimate(option.animate);
             $(window).scroll(function (event) {
@@ -17,6 +18,7 @@
                 }
             });
         },
+        /*设置动画*/
         setAnimate: function (animate) {
             if (animate) {
                 var eles = $(animate.ele);
@@ -25,8 +27,8 @@
                     for (var i = 0; i < eles.length; i++) {
                         var _top = $(window).height();
                         if (top + _top - eles[i].offsetTop >= $(eles[i]).height() - 80) {
-                            $(eles[i]).removeClass('transparent')
-                            $(eles[i]).children().addClass('animated fadeInUp')
+                            $(eles[i]).removeClass('transparent');
+                            $(eles[i]).children().addClass('animated fadeInUp');
                         }
                     }
                 }
