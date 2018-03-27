c3da7a28bb9912c480eafb166f9aff82cae457a5
1397648351 <wuzedc@163.com>
2018-03-27 22:36:27 +0800

    登陆静态完毕

--- public/static/dist/js/my.js
+++ public/static/dist/js/my.js
@@ -7,11 +7,19 @@
         my_init: function (option) {
             var default_option = {
                 animate: {
+                    enable: true,
                     ele: ".animated-end"
+                },
+                tabs: {
+                    enable: false,
+                    ele: ".form-type-tab",
+                    activeClass: 'tab-active',
+                    underLine: '.active-line'
                 }
             };
-            option = $.extend(default_option, option);
+            option = $.extend(true, default_option, option);
             $.setAnimate(option.animate);
+            $.setTabs(option.tabs);
             $(window).scroll(function (event) {
                 if (option.animate) {
                     $.setAnimate(option.animate);
@@ -20,7 +28,7 @@
         },
         /*设置动画*/
         setAnimate: function (animate) {
-            if (animate) {
+            if (animate.enable) {
                 var eles = $(animate.ele);
                 var top = $(window).scrollTop();
                 if (eles && eles.length > 0) {
@@ -34,5 +42,21 @@
                 }
             }
         },
+        setTabs: function (tabs) {
+            if (!tabs.enable) return;
+            $(tabs.ele).click(function () {
+                if ($(this).hasClass(tabs.activeClass)) return;
+                var actived_tab = $(tabs.ele + '.' + tabs.activeClass);
+                var actived_form = $(actived_tab.data('for'));
+                var underLine = $(this).parent().find(tabs.underLine);
+                var index=  $(this).parent().find(tabs.ele).index(this);
+                var count = $(this).parent().find(tabs.ele).length;
+                actived_tab.removeClass(tabs.activeClass);
+                $(this).addClass(tabs.activeClass);
+                actived_form.css('display', 'none');
+                $($(this).data('for')).css('display', 'block');
+                underLine.css('left', (100 * index / count) + '%');
+            })
+        }
     });
 })(jQuery);
