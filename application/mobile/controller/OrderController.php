e6433bb01b997d625cd41af0c7669313d28eff3f
light <982065980@qq.com>
2018-05-17 22:44:14 +0800

    【U】：order

--- application/mobile/controller/OrderController.php
+++ application/mobile/controller/OrderController.php
@@ -114,7 +114,7 @@ class OrderController extends PublicController
         //异步通知原样返回数据
         $payParam['return_param'] = 'pica';
         $payParam['product_id'] = $goods['id'];
-        $payParam['openid'] = session('openid');
+        $payParam['openid'] = 'oR38K1NwvjzceEjvmPiMvQyPbwno';
         return $payParam;
     }
 }
