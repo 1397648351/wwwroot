667059dda30d3385c60c26cc630868db5322019d
1397648351 <wuzedc@163.com>
2018-05-30 11:40:22 +0800

    微调

--- application/admin/model/GoodsOrder.php
+++ application/admin/model/GoodsOrder.php
@@ -14,12 +14,10 @@ class GoodsOrder extends Base
 {
     public function findAll($page, $rows)
     {
-        $map = array();
-        $map['a.status'] = 1;
-        $fields = 'a.out_trade_no as no,a.user_id,c.username,a.money,a.goods_id,b.subject,a.create_time,c.mobile,c.email,c.city,c.detail_address';
+        $fields = 'a.out_trade_no as no,a.user_id,c.username,a.money,a.goods_id,b.subject,a.create_time,c.mobile,c.email,c.city,c.detail_address,d.name as status';
         $offset = ($page - 1) * $rows;
         $total = $this->count();
-        $data = $this->alias('a')->join('goods b', 'a.goods_id=b.id', 'LEFT')->join('address c', 'a.id=c.goods_order_id', 'LEFT')->field($fields)->where($map)->order('a.create_time', 'desc')->limit($offset, $rows)->select();
+        $data = $this->alias('a')->join('goods b', 'a.goods_id=b.id', 'LEFT')->join('address c', 'a.id=c.goods_order_id', 'LEFT')->join('status d', 'a.status=d.id', 'LEFT')->field($fields)->order('a.create_time', 'desc')->limit($offset, $rows)->select();
         $result['total'] = $total;
         $result['data'] = $data;
         return $result;
@@ -27,10 +25,8 @@ class GoodsOrder extends Base
 
     public function getOrderList()
     {
-        $map = array();
-        $map['a.status'] = 1;
-        $fields = 'a.out_trade_no as no,a.user_id,c.username,a.money,a.goods_id,b.subject,a.create_time,c.mobile,c.email,c.city,c.detail_address';
-        $data = $this->alias('a')->join('goods b', 'a.goods_id=b.id', 'LEFT')->join('address c', 'a.id=c.goods_order_id', 'LEFT')->field($fields)->where($map)->order('a.create_time', 'desc')->select();
+        $fields = 'a.out_trade_no as no,a.user_id,c.username,a.money,a.goods_id,b.subject,a.create_time,c.mobile,c.email,c.city,c.detail_address,d.name as status';
+        $data = $this->alias('a')->join('goods b', 'a.goods_id=b.id', 'LEFT')->join('address c', 'a.id=c.goods_order_id', 'LEFT')->join('status d', 'a.status=d.id', 'LEFT')->field($fields)->order('a.create_time', 'desc')->select();
         return $data;
     }
 }
